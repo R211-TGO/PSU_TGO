972c4c93ae6d1439189a255b60e08289b0ea4c73
KIM2548TH <jitkon1979@gmail.com>
2025-05-19 12:55:04 +0700

    add demo model for database

---
+++ webapp/models/materail_model.py
@@ -0,0 +1,25 @@
+import mongoengine as me
+import datetime
+
+class QuantityType(me.EmbeddedDocument):
+    field = me.StringField(required=True)
+    label = me.StringField(required=True)
+    amount = me.FloatField(required=True)
+    unit = me.StringField(required=True)
+
+class Material(me.Document):
+    name = me.StringField(required=True)
+    scope = me.StringField(required=True)
+    form_and_formula = me.StringField(required=True)
+    quantity_type = me.EmbeddedDocumentListField(QuantityType)
+    year = me.IntField(required=True)
+    month = me.IntField(required=True)
+    day = me.IntField(required=True)
+    result = me.DynamicField(default=None)  # ใช้ DynamicField เพื่อรองรับค่า null หรือค่าประเภทอื่น
+    create_date = me.DateTimeField(default=datetime.datetime.now)
+    update_date = me.DateTimeField(default=datetime.datetime.now)
+
+    meta = {
+        "collection": "materials",
+        "indexes": ["name", "scope", "year", "month", "day"]
+    }
